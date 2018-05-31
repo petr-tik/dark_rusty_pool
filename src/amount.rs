84811528d7047a299e272e0030b44f01249668bc
petr-tik <petr-tik@users.noreply.github.com>
2018-05-31 23:33:39 +0100

    Implemented the Amount module to use for the price type
    Unsigned as price can never be negative, 32-bit, because I don't
    expect us to need more memory.
    
    Added unit tests inside the module, run with
    
    cargo test -- amount

---
+++ src/amount.rs
@@ -0,0 +1,84 @@
+use std::fmt::Display;
+use std::fmt::Formatter;
+use std::fmt::Result;
+use std::num::ParseFloatError;
+use std::ops::AddAssign;
+use std::ops::MulAssign;
+
+pub struct Amount {
+    as_int: u32,
+}
+
+impl Amount {
+    fn new(input_string: &str) -> Self {
+        let float_from_input = input_string.parse::<f32>();
+        let float_res = match float_from_input {
+            Ok(number_to_round) => number_to_round,
+            Err(ParseFloatError) => panic!("Input string {} doesn't parse as f32", input_string),
+        };
+        let float_times_hundred = float_res * 100.0;
+        let int_res = float_times_hundred.round() as u32;
+        return Amount { as_int: int_res };
+    }
+}
+
+impl AddAssign for Amount {
+    fn add_assign(&mut self, other_amount: Self) {
+        self.as_int += other_amount.as_int;
+    }
+}
+
+impl MulAssign<u32> for Amount {
+    fn mul_assign(&mut self, multiplier: u32) {
+        self.as_int *= multiplier;
+    }
+}
+
+impl Display for Amount {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        let quot_x = self.as_int.checked_div(100).unwrap();
+        let rem_x = self.as_int.checked_rem(100).unwrap();
+
+        write!(f, "{}.{}", quot_x, rem_x)
+    }
+}
+
+// todo - turn price into unsigned value
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn constructor_works() {
+        let am = Amount::new(&"44.12");
+        assert_eq!(am.as_int, 4412);
+    }
+
+    #[test]
+    #[should_panic]
+    fn bad_constructor_panics() {
+        Amount::new(&"asda");
+    }
+
+    #[test]
+    fn multiply_by_zero() {
+        let mut am = Amount::new(&"44.12");
+        am *= 0;
+        assert_eq!(am.as_int, 0);
+    }
+
+    #[test]
+    fn multiply_by_ten() {
+        let mut am = Amount::new(&"44.12");
+        am *= 10;
+        assert_eq!(am.as_int, 44120);
+    }
+
+    #[test]
+    fn add_two_amounts() {
+        let mut am1 = Amount::new(&"44.12");
+        let am2 = Amount::new(&"45.80");
+        am1 += am2;
+        assert_eq!(am1.as_int, 8992);
+    }
+}
