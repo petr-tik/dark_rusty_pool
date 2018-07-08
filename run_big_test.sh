1a621d8b399a5cd3b58fb8d4028522cf1cc0db22
petr-tik <petr-tik@users.noreply.github.com>
2018-07-08 22:33:40 +0100

    testing: added bash to text for another target size
    Compares to expected output

--- run_big_test.sh
+++ run_big_test.sh
@@ -1,6 +1,11 @@
 gunzip data/pricer.in.gz
 cargo test
 cargo build --release
-echo "Running release binary with 200 as target size and saving output to /tmp/rust_pricer"
-cargo run --release 200 < data/pricer.in > /tmp/rust_pricer
+echo "Running release binary with 200 as target size and saving output to /tmp/rust_pricer_out_200"
+cargo run --release 200 < data/pricer.in > /tmp/rust_pricer_out_200
+zcat data/pricer.out.200.gz | diff -s /tmp/rust_pricer_out -
 echo "to view results, cat/head /tmp/rust_pricer"
+echo "Running release binary with 10000 as target size and saving output to /tmp/rust_pricer_out_10000"
+cargo run --release 10000 < data/pricer.in > /tmp/rust_pricer_out_10000
+zcat data/pricer.out.10000.gz | diff -s /tmp/rust_pricer_out_10000 -
+
--- time_rust_pricer.sh
+++ time_rust_pricer.sh
@@ -1,4 +1,8 @@
 cargo test -q
 cargo build -q --release
 free && sync && sudo sh -c "echo 3 > /proc/sys/vm/drop_caches" && free
+echo "Benchmarking with target_size: 200"
 time ./target/release/order_book 200 < data/pricer.in &> /dev/null
+free && sync && sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+echo "Benchmarking with target_size: 10000"
+time ./target/release/order_book 10000 < data/pricer.in &> /dev/null
