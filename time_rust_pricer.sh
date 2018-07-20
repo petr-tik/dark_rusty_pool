4de769f58fc0c16f27a4c2d247164cccb6046fe5
petr-tik <petr-tik@users.noreply.github.com>
2018-07-20 01:20:59 +0100

    perf: collect use perf stat instead of time

--- time_rust_pricer.sh
+++ time_rust_pricer.sh
@@ -2,7 +2,7 @@ cargo test -q
 cargo build -q --release
 free && sync && sudo sh -c "echo 3 > /proc/sys/vm/drop_caches" && free
 echo "Benchmarking with target_size: 200"
-time ./target/release/order_book 200 < data/pricer.in &> /dev/null
+sudo perf stat -e cpu-clock,task-clock,cs,cache-references,cache-misses,branches,branch-misses ./target/release/order_book 200 < data/pricer.in > /dev/null 
 free && sync && sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
 echo "Benchmarking with target_size: 10000"
-time ./target/release/order_book 10000 < data/pricer.in &> /dev/null
+sudo perf stat -e cpu-clock,task-clock,cs,cache-references,cache-misses,branches,branch-misses ./target/release/order_book 10000 < data/pricer.in > /dev/null 
